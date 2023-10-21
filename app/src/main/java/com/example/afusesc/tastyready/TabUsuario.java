afe0da98f4d2b2c4fd2c34e34494e661a53e39c5
Hao Xu <hxu2@epsg.upv.es>
2023-10-21 18:42:02 +0200

    Intento Login

--- app/src/main/java/com/example/afusesc/tastyready/TabUsuario.java
+++ app/src/main/java/com/example/afusesc/tastyready/TabUsuario.java
@@ -1,20 +1,56 @@
 package com.example.afusesc.tastyready;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.LruCache;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class TabUsuario extends Fragment {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
+public class TabUsuario extends AppCompatActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_user);
+
+        FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+
+        TextView nombre = findViewById(R.id.nombre);
+        nombre.setText(usuario.getDisplayName());
+        TextView coreo = findViewById(R.id.correo);
+        coreo.setText(usuario.getEmail());
+        TextView prove = findViewById(R.id.proveedor);
+        prove.setText(usuario.getProviderId());
+        TextView telf = findViewById(R.id.telefono);
+        telf.setText(usuario.getPhoneNumber());
+        TextView Uid = findViewById(R.id.uid);
+        Uid.setText(usuario.getUid());
+
+        // Inicialización Volley (Hacer solo una vez en Singleton o Applicaction)
+        RequestQueue colaPeticiones = Volley.newRequestQueue(this); ImageLoader lectorImagenes = new ImageLoader(colaPeticiones, new ImageLoader.ImageCache() { private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(10); public void putBitmap(String url, Bitmap bitmap) {cache.put(url, bitmap); } public Bitmap getBitmap(String url) { return cache.get(url); } }); Uri urlImagen = usuario.getPhotoUrl(); if (urlImagen != null) { NetworkImageView foto = (NetworkImageView) findViewById(R.id.imagen); foto.setImageUrl(urlImagen.toString(), lectorImagenes); }
+
     }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_user, container, false);
-    }
+    public void cerrarSesion(View view) {
+        AuthUI.getInstance().signOut(getApplicationContext()) .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override public void onComplete(@NonNull Task<Void> task) {
+                Intent i = new Intent( getApplicationContext (),LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); startActivity(i); finish(); } }); }
+
+
 }
